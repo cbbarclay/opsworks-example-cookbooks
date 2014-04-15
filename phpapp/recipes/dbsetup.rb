0d112b9c400f487d2110371764be29f41550faa2
cbbarclay <cbbarclay@hotmail.com>
2014-04-15 10:40:27 -0700

    Update dbsetup.rb

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -1,12 +1,12 @@
 node[:deploy].each do |app_name, deploy|
   execute "mysql-create-table" do
-    command "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{deploy[:database][:table]}(
+    command "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{deploy[:application]}(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     author VARCHAR(63) NOT NULL,
     message TEXT,
     PRIMARY KEY (id)
   )'"
-    not_if "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{deploy[:database][:table]}"
+    not_if "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{deploy[:application]}"
     action :run
   end
 end
