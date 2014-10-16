89a09a0cf6aba66aa3aaef5f0bd1a7c7b1c12ca6
cbbarclay <cbbarclay@hotmail.com>
2014-10-16 16:02:06 -0700

    Update create-user.rb

--- haproxy/recipes/create-user.rb
+++ haproxy/recipes/create-user.rb
@@ -1,4 +1,4 @@
-execute mysql_check do
+execute "mysql_check" do
   node[:deploy].each do |application, deploy|
     command "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'INSERT INTO mysql.user (Host,User) values (\'#{node[:opsworks][:instance][:private_ip]}\',\'haproxy_check\'); FLUSH PRIVILEGES;'"
   end
