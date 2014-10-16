899d51c405e04bcb1be71d095ac00719a9f9c1f4
cbbarclay <cbbarclay@hotmail.com>
2014-10-16 16:41:03 -0700

    Update create-user.rb

--- haproxy/recipes/create-user.rb
+++ haproxy/recipes/create-user.rb
@@ -1,3 +1,5 @@
+# TODO: need to add logic if this IP has already been used
+
 execute "mysql_check" do
   node[:deploy].each do |application, deploy|
     command "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'INSERT INTO mysql.user (Host,User) values (\"#{node[:opsworks][:instance][:private_ip]}\",\"haproxy_check\"); FLUSH PRIVILEGES;'"
